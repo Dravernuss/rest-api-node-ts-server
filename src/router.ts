2a37bb7afbf0a4faae9d4cc73ee54431c5c18f10
Dravernuss <esteban16.rodas@gmail.com>
2025-02-15 14:30:50 -0500

    Creating REST api for products

---
+++ src/router.ts
@@ -0,0 +1,73 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  getProductsById,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/product";
+import { handleInputErrors } from "./middleware";
+
+const router = Router();
+
+router.get("/", getProducts);
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  getProductsById
+);
+
+router.post(
+  "/",
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del Producto no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio del Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio inválido"),
+  handleInputErrors,
+  createProduct
+);
+
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del Producto no puede ir vacio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio del Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio inválido"),
+  body("availability")
+    .isBoolean()
+    .withMessage("Valor para disponibilidad no válido"),
+  handleInputErrors,
+  updateProduct
+);
+
+router.patch(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  updateAvailability
+);
+
+router.delete(
+  "/:id",
+  param("id").isInt().withMessage("ID no valido"),
+  handleInputErrors,
+  deleteProduct
+);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Express from "express";
+import colors from "colors";
+import router from "./router";
+import db from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Conexion exitosa a la BD"));
+  } catch (error) {
+    console.log(error);
+    console.log(colors.red.bold("Hubo un error al conectar a la BD"));
+  }
+}
+connectDB();
+
+const server = Express();
+
+server.use(Express.json());
+
+server.use("/api/products", router);
+
+export default server;
